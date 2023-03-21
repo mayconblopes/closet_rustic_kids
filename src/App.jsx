e35c7bad48f1fe5324694934524b57e790cc41fd
Maycon Lopes <lopes.maycon@gmail.com>
2023-03-21 07:17:38 -0300

    Starting App

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './categories.styles.scss'
+
+const categories = [
+  {
+    id: 1,
+    title: 'Hats',
+  },
+
+  {
+    id: 2,
+    title: 'Jackets',
+  },
+
+  {
+    id: 3,
+    title: 'Sneakers',
+  },
+
+  {
+    id: 4,
+    title: 'Womens',
+  },
+
+  {
+    id: 5,
+    title: 'Mens',
+  },
+]
+
+function App() {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <div key={category.id} className="category-container">
+          <div className='background-image'></div>
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
