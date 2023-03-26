0ee5535430c2a3731afb25aaeebef2e1a704af4b
Maycon Lopes <lopes.maycon@gmail.com>
2023-03-26 15:04:26 -0300

    refactored category component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import './category-item.styles.scss'
+import './directory-item.styles.scss'
 
-function CategoryItem({ category }) {
+function DirectoryItem({ category }) {
   return (
-    <div className='category-container'>
+    <div className='directory-item-container'>
       <div
         className='background-image'
         style={{ backgroundImage: `url(${category.imageUrl})` }}
       ></div>
-      <div className='category-body-container'>
+      <div className='body'>
         <h2>{category.title}</h2>
         <p>Shop Now</p>
       </div>
     </div>
   )
 }
-export default CategoryItem
+export default DirectoryItem
